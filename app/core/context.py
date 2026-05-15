67e587f92bb80877224e96a8873a0448a203e1f7
Amr Mohsen <algoshari991010@gmail.com>
2026-05-15 20:54:48 +0300

    did nothing

--- app/core/context.py
+++ app/core/context.py
@@ -2,6 +2,7 @@ from flask_login import current_user
 from flask import request
 from app.presentation.context.user import get_user_context
 from app.presentation.context.layout import get_layout_context
+from app.presentation.context.newsletter import get_newsletter_context
 from app.presentation.context.filters import get_filter_url, get_sort_url, get_page_url
 
 
